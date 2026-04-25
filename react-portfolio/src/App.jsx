c9ae0986ad884a537412df9dc8bca266902772df
joel06rob <joel.roberts2006@gmail.com>
2026-04-25 20:43:59 +0100

    page setup

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,6 +1,21 @@
 
 function App() {
-    
+  return(
+    <div className="min-h-screen bg-black text-white">
+      <div className="absolute inset-0 animate-pulseGrid"
+      style={{
+      backgroundImage: `
+        linear-gradient(rgba(15, 37, 125,0.1) 1px, transparent 1px),
+        linear-gradient(90deg,rgba(15, 37, 125,0.1) 1px, transparent 1px)
+      `,
+      backgroundSize: "40px 40px"
+      }}>
+        <div className="max-w-5xl mx-auto px-4">
+          
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App
