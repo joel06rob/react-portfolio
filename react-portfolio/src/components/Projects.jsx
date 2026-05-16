f6af02ad6c9d016d983f5e721cb3a29dacfb5865
joel06rob <joel.roberts2006@gmail.com>
2026-05-16 10:24:24 +0100

    updates

--- react-portfolio/src/components/Projects.jsx
+++ react-portfolio/src/components/Projects.jsx
@@ -29,8 +29,8 @@ export default function Projects(){
         <p className="pt-20 pb-6">Projects:</p>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-1 md:gap-3">
             {projects.map((proj, index) => (
-            <div key={index} className="bg-[#161617]/55 p-6 rounded-lg mb-6 hover:bg-[#3d3d3d]/50 transition-colors duration-300">
-                <a href={proj.link}>
+            <a key={index} href={proj.link} className="bg-[#161617]/55 p-6 rounded-lg mb-6 hover:bg-[#3d3d3d]/50 transition-colors duration-300">
+                
                 <img src={proj.img} className="rounded-lg pb-2"></img>
                 <h3 className="text-lg md:text-xl font-semibold">{proj.title}</h3>
                 <p className="text-sm md:text-base text-gray-400 mb-4">{proj.desc}</p>
@@ -42,8 +42,8 @@ export default function Projects(){
                     </span>
                 ))}
                 </div>
-                </a>
-            </div>
+            </a>
+            
             ))}
             <a href="https://github.com/joel06rob/" className="bg-[#161617]/55 p-6 rounded-lg mb-6 hover:bg-[#3d3d3d]/50 transition-colors duration-300">
                 <img src="/icons8-github.svg" className="pb-2"></img>
